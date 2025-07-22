eb6298168ae8d74ce3eb1f89dad1bbaf55ff4408
Github Action <noreply@github.com>
2025-07-22 17:54:23 +0000

    Update dependencies.

--- golang/binder/cgx/cgx.cgo.h
+++ golang/binder/cgx/cgx.cgo.h
@@ -21,11 +21,13 @@ typedef struct { const char *p; ptrdiff_t n; } _GoString_;
 
 #line 37 "cgx.go"
 
+
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-#include <gx/golang/binder/cgx/cgx.h>
+#include <gxdeps/github.com/gx-org/gx/golang/binder/cgx/cgx.h>
 
 // cgx_device_get_result is the return value for cgx_device_get().
 struct cgx_device_get_result {
