75885bf0fdcd1dd861a5b6ffeb517fc293270fa8
Github Action <noreply@github.com>
2025-12-09 17:10:04 +0000

    Update dependencies.

--- examples/linearregression/linearregression/linearregression.go
+++ examples/linearregression/linearregression/linearregression.go
@@ -1,10 +1,10 @@
-// Package basic encapsulates GX source files
+// Package linearregression encapsulates GX source files
 // into a Go package.
 //
 // Automatically generated from google3/third_party/gxlang/gx/golang/packager/package.go.
 //
 // DO NOT EDIT
-package basic
+package linearregression
 
 import (
 	"embed"
@@ -14,20 +14,20 @@ import (
 
 )
 
-//go:embed basic.gx 
+//go:embed learner.gx target.gx 
 var srcs embed.FS
 
 var inputFiles = []string{
-"basic.gx",
+"learner.gx","target.gx",
 }
 
 func init() {
-	embedpkg.RegisterPackage("github.com/gx-org/gx/tests/bindings/basic", Build)
+	embedpkg.RegisterPackage("github.com/gx-org/gx/examples/linearregression/linearregression", Build)
 }
 
 var _ embedpkg.BuildFunc = Build
 
 // Build GX package.
 func Build(bld *builder.Builder) (builder.Package, error) {
-	return bld.BuildFiles("github.com/gx-org/gx/tests/bindings", "basic", srcs, inputFiles)
+	return bld.BuildFiles("github.com/gx-org/gx/examples/linearregression", "linearregression", srcs, inputFiles)
 }
