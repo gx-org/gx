bfa5584f89fda76cba6c2857b9ff3520f44b9d49
Github Action <noreply@github.com>
2025-12-18 09:59:55 +0000

    Update dependencies.

---
+++ golang/backend/tests/notimp/notimp.go
@@ -0,0 +1,33 @@
+// Package notimp encapsulates GX source files
+// into a Go package.
+//
+// Automatically generated from google3/third_party/gxlang/gx/golang/packager/package.go.
+//
+// DO NOT EDIT
+package notimp
+
+import (
+	"embed"
+
+	"github.com/gx-org/gx/build/builder"
+	"github.com/gx-org/gx/build/importers/embedpkg"
+
+)
+
+//go:embed notimp.gx 
+var srcs embed.FS
+
+var inputFiles = []string{
+"notimp.gx",
+}
+
+func init() {
+	embedpkg.RegisterPackage("github.com/gx-org/gx/golang/backend/tests/notimp", Build)
+}
+
+var _ embedpkg.BuildFunc = Build
+
+// Build GX package.
+func Build(bld *builder.Builder) (builder.Package, error) {
+	return bld.BuildFiles("github.com/gx-org/gx/golang/backend/tests", "notimp", srcs, inputFiles)
+}
