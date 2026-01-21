8b44761f3fa4cd19040c555efa555cfc5962a6a7
Googler <noreply@google.com>
2026-01-21 08:44:24 -0800

    Project import generated by Copybara.
    PiperOrigin-RevId: 859119114

--- internal/interp/compeval/cpevelements/annotator.go
+++ internal/interp/compeval/cpevelements/annotator.go
@@ -20,55 +20,55 @@ import (
 	"github.com/gx-org/gx/interp/fun"
 )
 
-// Annotator is a macro function to build synthetic functions.
-type Annotator struct {
+// FuncAnnotator is a macro function to build synthetic functions.
+type FuncAnnotator struct {
 	ann  *ir.Annotator
 	recv *fun.Receiver
 }
 
-var _ ir.FuncAnnotator = (*Annotator)(nil)
+var _ ir.FuncAnnotator = (*FuncAnnotator)(nil)
 
 // NewAnnotator creates a new macro given its definition and a receiver.
 func NewAnnotator(fn *ir.Annotator, recv *fun.Receiver) fun.Func {
-	return &Annotator{ann: fn, recv: recv}
+	return &FuncAnnotator{ann: fn, recv: recv}
 }
 
 // Func returns the macro function.
-func (f *Annotator) Func() ir.Func {
+func (f *FuncAnnotator) Func() ir.Func {
 	return f.ann
 }
 
 // Recv returns the receiver of the macro function.
-func (f *Annotator) Recv() *fun.Receiver {
+func (f *FuncAnnotator) Recv() *fun.Receiver {
 	return f.recv
 }
 
 // Call the macro to build the synthetic element.
-func (f *Annotator) Call(fctx *fun.CallEnv, call *ir.FuncCallExpr, args []ir.Element) ([]ir.Element, error) {
+func (f *FuncAnnotator) Call(fctx *fun.CallEnv, call *ir.FuncCallExpr, args []ir.Element) ([]ir.Element, error) {
 	return nil, errors.Errorf("annotator gx:@%s only valid in a function annotation context", f.Name())
 }
 
 // IR of the macro function.
-func (f *Annotator) IR() ir.Func {
+func (f *FuncAnnotator) IR() ir.Func {
 	return f.ann
 }
 
 // Name of the annotator.
-func (f *Annotator) Name() string {
+func (f *FuncAnnotator) Name() string {
 	return f.ann.File().Package.Name.Name + "." + f.ann.Name()
 }
 
 // ShortString returns the name of the annotator.
-func (f *Annotator) ShortString() string {
+func (f *FuncAnnotator) ShortString() string {
 	return f.Name()
 }
 
 // Annotate a given function for a given call.
-func (f *Annotator) Annotate(fetcher ir.Fetcher, fn ir.PkgFunc, call *ir.FuncCallExpr, args []ir.Element) bool {
+func (f *FuncAnnotator) Annotate(fetcher ir.Fetcher, fn ir.PkgFunc, call *ir.FuncCallExpr, args []ir.Element) bool {
 	return f.ann.Annotate(fetcher, f.ann, fn, call, args)
 }
 
 // Type returns the type of the function.
-func (f *Annotator) Type() ir.Type {
+func (f *FuncAnnotator) Type() ir.Type {
 	return f.ann.Type()
 }
