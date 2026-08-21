222c6d0fb66a5f1fedc6e506201c2b00048a042f
Github Action <noreply@github.com>
2026-08-21 13:55:46 +0000

    Update dependencies.

---
+++ tests/bindings/generics/generics.go
@@ -0,0 +1,33 @@
+// Package generics encapsulates GX source files
+// into a Go package.
+//
+// Automatically generated from google3/third_party/gxlang/gx/golang/packager/package.go.
+//
+// DO NOT EDIT
+package generics
+
+import (
+	"embed"
+
+	"github.com/gx-org/gx/build/importers"
+	"github.com/gx-org/gx/build/importers/embedpkg"
+
+)
+
+//go:embed generics.gx 
+var srcs embed.FS
+
+var inputFiles = []string{
+"generics.gx",
+}
+
+func init() {
+	embedpkg.RegisterPackage("github.com/gx-org/gx/tests/bindings/generics", Build)
+}
+
+var _ embedpkg.BuildFunc = Build
+
+// Build GX package.
+func Build(bld importers.Builder) (importers.Package, error) {
+	return bld.BuildFiles("github.com/gx-org/gx/tests/bindings", "generics", srcs, inputFiles)
+}
