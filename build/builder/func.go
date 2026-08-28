abededa346f1ede44adcacc167a5ac0ceaaca00f
Googler <noreply@google.com>
2026-08-28 07:04:43 -0700

    Project import generated by Copybara.
    PiperOrigin-RevId: 972568880

--- build/builder/func.go
+++ build/builder/func.go
@@ -160,6 +160,9 @@ func (n *funcType) buildFuncType(rscope resolveScope) (*ir.FuncType, *funcResolv
 	if !tParamsOk {
 		return ext, nil, false
 	}
+	if ext.CompEval && ext.TypeParams.Len() > 0 {
+		tParamsOk = rscope.Err().Appendf(ext.TypeParams.Node(), "compeval functions cannot have generic parameters")
+	}
 	ext.Receiver, recvOk = n.recv.buildFieldList(newDefineScope(sigscope, nil))
 	if recvOk && ext.Receiver != nil {
 		if field := ext.ReceiverField(); field.Name != nil {
@@ -188,7 +191,7 @@ func (n *funcType) buildFuncType(rscope resolveScope) (*ir.FuncType, *funcResolv
 		}
 	}
 	fnscope, fnscopeOk := newFuncScope(rscope, ext)
-	return ext, fnscope, resultsOk && fnscopeOk
+	return ext, fnscope, resultsOk && fnscopeOk && tParamsOk
 }
 
 func (n *funcType) source() ast.Node {
